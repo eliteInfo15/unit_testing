87c40b2452d3c1abc134ba641f17ac92db815347
eliteInfo15 <40851341+eliteInfo15@users.noreply.github.com>
2023-04-30 13:37:29 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+export class Cube {
+    constructor(length) 
+    {
+        this.length = length;
+    }
+    
+    getSideLength () {
+    return this.length;
+    }
+    
+    getSurfaceArea () {
+    return (this.length * this.length) * 6;
+    }
+    
+    getVolume () {
+    return Math.pow(this.length,3);
+    }
+    }

0b09682701880fe73810cac7b2edf735a7365f03
jitesh <jitesh.bharad0@gmail.com>
2023-04-30 13:24:34 +0530

    Added unit testing working project

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+import { Cube } from '../src/app.js';
+import { expect } from 'chai';
+
+describe('Testing the Cube Functions', function() {
+it('1. The side length of the Cube', function(done) {
+let c1 = new Cube(2);
+expect(c1.getSideLength()).to.equal(2);
+done();
+});
+
+
+it('3. The volume of the Cube', function(done) {
+let c3 = new Cube(7);
+expect(c3.getVolume()).to.equal(343);
+done();
+});
+
+});
